5c16475bf328ec404d8167a93b4c3870618c4986
litc0de <litc0de@posteo.org>
2024-10-28 13:54:24 +0100

    create sounds

--- src/tone/sounds/Sound3.ts
+++ src/tone/sounds/Sound3.ts
@@ -1,43 +1,51 @@
 import BaseSound from "./BaseSound";
 import MyMixer from "../Mixer";
-import { Add, LFO, Multiply, Oscillator } from "tone";
+import { AMOscillator, AutoFilter, LFO, getTransport } from "tone";
 import { Frequency, Time } from "tone/build/esm/core/type/Units";
 
 class Sound3 extends BaseSound {
-  saw = new Oscillator("A3", "sawtooth");
-  // loop: Loop;
-  lfo1 = new LFO({ frequency: "1m" });
-  lfo2 = new LFO();
-  mainFreq: Frequency = "A4";
-  mainOsc = new Oscillator(this.mainFreq);
-  scale = new Multiply(50);
-  add = new Add(100);
+  mainFreq: Frequency = "F2";
+  mainOsc = new AMOscillator(this.mainFreq, "sine", "square");
+  lfo = new LFO("1m", 2, 27);
+  filter = new AutoFilter({ frequency: "5hz", depth: 0.6 });
 
   constructor(mixer: MyMixer) {
     super(mixer);
     this.init();
-
-    // this.loop = new Loop(() => {
-    //   this.mainOsc.set({ phase: 0 });
-    // }, "154hz");
   }
 
   init(): void {
-    this.mainOsc.connect(this.mixer.channel3);
+    this.mainOsc.chain(this.filter, this.mixer.channel3);
     this.mixer.channel3.set({ volume: -10 });
-    // this.lfo1.chain(this.scale, this.add, this.mainOsc.frequency);
+    this.lfo.chain(this.mainOsc.detune);
+
+    getTransport().scheduleRepeat((time) => {
+      this.mainOsc.harmonicity.setValueAtTime(0.5, time);
+      // this.mainOsc.harmonicity.linearRampToValueAtTime(2.5, time + 1);
+      // this.mainOsc.harmonicity.linearRampToValueAtTime(0.5, time + 2);
+      this.mainOsc.harmonicity.setValueAtTime(1.5, time + 1);
+      this.mainOsc.harmonicity.setValueAtTime(0.25, time + 2);
+      this.mainOsc.harmonicity.setValueAtTime(1.75, time + 3);
+      this.mainOsc.harmonicity.setValueAtTime(1, time + 4);
+      // this.mainOsc.harmonicity.linearRampToValueAtTime(10, time + 3);
+      // this.mainOsc.harmonicity.linearRampToValueAtTime(1, time + 4);
+
+      // this.mainOsc.detune.setValueAtTime(0, time);
+      // this.mainOsc.detune.linearRampToValueAtTime(20, time + 2);
+      // this.mainOsc.detune.linearRampToValueAtTime(0, time + 4);
+    }, 4);
   }
 
   play(time: Time) {
     this.mainOsc.start(time);
-    // this.lfo1.start();
-    // this.loop.start();
+    this.lfo.start(time);
+    this.filter.start(time);
   }
 
   stop() {
-    this.lfo1.stop();
-    // this.loop.stop();
     this.mainOsc.stop();
+    this.lfo.stop();
+    this.filter.stop();
   }
 }
 
