0806d79eaab8c6f5b62ad518048c08a14effde99
litc0de <litc0de@posteo.org>
2025-01-18 13:49:43 +0100

    add active indicator

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -9,7 +9,12 @@ const Layout: React.FC<Props> = ({ children }) => {
   return (
     <>
       <nav>
-        <Link to="/">Home</Link> <Link to="/stories">Info</Link>
+        <Link to="/" className={(active) => (active ? "active" : "")}>
+          Home
+        </Link>{" "}
+        <Link to="/stories" className={(active) => (active ? "active" : "")}>
+          Info
+        </Link>
       </nav>
       <main className="box-border flex h-screen w-screen flex-col gap-8">{children}</main>
     </>
